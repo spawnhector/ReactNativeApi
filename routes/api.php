46e61354d8ed840f6df37c8738f4f07596639661
spawnhector <spawnhector@gmail.com>
2022-03-20 12:49:15 -0500

    test api

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('test',function(){
-    return response(
-        $data = "verified"
-    );
+    return 'test';
 });
