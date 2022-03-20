df9b98f544fd2eae12c66bb73824e8da168c81ed
spawnhector <spawnhector@gmail.com>
2022-03-20 12:57:21 -0500

    tret

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('test',function(){
+Route::get('/test',function(){
     return 'test';
 });
